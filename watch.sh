a2617dee1f22f7c9f6f0088b2c4f7af236730ce4
Antoine Viau <contact@antoineviau.com>
2020-01-09 19:04:34 +0100

    Ajout de watch pour dev.

---
+++ watch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# set -ex
+php src/generate.php site > src/index.html
+./node_modules/.bin/static src &
+inotifywait -m -q -r -e close_write ./src --exclude index.html |
+while read -r filename event; do
+	php src/generate.php site > src/index.html
+done
+
+
